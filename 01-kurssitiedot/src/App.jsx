d879850a9d58391466825538f3617e0ecab1868b
alex Gainullin <againullin8@gmail.com>
2024-02-09 14:25:35 +0200

    1.3: course information step3

--- 01-kurssitiedot/src/App.jsx
+++ 01-kurssitiedot/src/App.jsx
@@ -1,4 +1,5 @@
-//1.2: kurssitiedot, step2
+//1.3: course information step3
+
 import React from 'react';
 
 const Header = (props) => {
@@ -32,25 +33,28 @@ const Content = (props) => {
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
-
-  const totalExercises = exercises1 + exercises2 + exercises3;
-
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  const totalExercises = (part1.exercises + part2.exercises + part3.exercises);
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content part={part1.name} exercises={part1.exercises} />
+      <Content part={part2.name} exercises={part2.exercises} />
+      <Content part={part3.name} exercises={part3.exercises} />
       <Total totalExercises={totalExercises} />
     </div>
   );
 };
-
 export default App;
