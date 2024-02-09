8211885ab1c823fe98357508e416b0c349f16dfb
alex Gainullin <againullin8@gmail.com>
2024-02-09 14:19:18 +0200

    1.1: kurssitiedot, step1

---
+++ 01-kurssitiedot/src/App.jsx
@@ -0,0 +1,49 @@
+//1.1: kurssitiedot, step1
+const Header = (props) => {
+  return (
+    <h1>
+      {props.course}
+    </h1>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <p>
+      {props.part1} {props.exercises1}
+      {props.part2} {props.exercises2}
+      {props.part3} {props.exercises3}
+    </p>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {props.totalExercises}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
+  const totalExercises = exercises1 + exercises2 + exercises3;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1} />
+      <Content part2={part2} exercises2={exercises2} />
+      <Content part3={part3} exercises3={exercises3} />
+      <Total totalExercises={totalExercises} />
+    </div>
+  );
+};
+ export default App;
