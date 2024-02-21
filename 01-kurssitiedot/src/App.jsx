138b45de44e5bc87047b67c172006d702378ada7
alex Gainullin <againullin8@gmail.com>
2024-02-21 18:38:12 +0200

    1.9: unicafe step4

--- 01-kurssitiedot/src/App.jsx
+++ 01-kurssitiedot/src/App.jsx
@@ -1,4 +1,4 @@
-//1.8: unicafe step3
+//1.9: unicafe step4 
 import { useState } from 'react'
 
 const Statistics = ({ good, neutral, bad }) => {
@@ -6,6 +6,7 @@ const Statistics = ({ good, neutral, bad }) => {
   const average = (good - bad)/all
   const positive = (good / all)*100
 return(
+  
  <div>
    <h1>statistics</h1>
     <p>good: {good}</p>
@@ -24,20 +25,29 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
- 
+  
+  
+  const feedbackGiven = good !== 0 || neutral !== 0 || bad !== 0
 
   return (
-     <div>
+    <div>
       <h1>give feedback</h1>
-       <button onClick={() => setGood(good + 1)}>good</button> 
-       <button onClick={() => setNeutral(neutral + 1)}>neutral</button> 
-       <button onClick={() => setBad(bad + 1)}>bad</button>
-       <Statistics good={good} neutral={neutral} bad={bad} />
-    
-  </div>
-  ) }
-  
-  export default App
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      {feedbackGiven ? (
+        <div>
+          
+          <Statistics good={good} neutral={neutral} bad={bad} />
+        </div>
+      ) : (
+        <div>
+          <h1>statistics</h1>
+          <p>No feedback given</p>
+        </div>
+      )}
+    </div>
+  )
+}
 
-
-
+export default App
